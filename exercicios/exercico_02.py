7ed571f2c49f7f1846a7235d4d8e4a8fbc40328a
Rodrigo55pereira <rodrigo55pereira@gmail.com>
2020-09-05 17:45:12 -0300

    exe 1 exe 2

---
+++ exercicios/exercico_02.py
@@ -0,0 +1,5 @@
+# Execicio 02
+
+nome = input('Qual o seu nome?\n')
+idade = int(input('Qual a sua idade?\n'))
+print(f'Bem vindo {nome}, fico feliz em saber que tem {idade} anos.')
---
+++ explicacoes/print_input.py
@@ -0,0 +1,12 @@
+# print <- Função que escreve na tela, quando chama
+# print('Oi, meu nome é Rodrigo\n')
+# print('Oi, meu nome é Fausto')
+
+# input <- Escreve uma mensagem e espera que você escreva
+
+idade = input('Qual a sua idade?')
+
+print(idade)
+
+
+
